68a34ce0ed2338cc26fa18e5bed10b9288549ba4
Lucas Gabriel de Lana Rosa <lucas.gabriel.lr2112@gmail.com>
2025-04-06 13:23:30 -0300

    added: Header Component & App.module.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { useSaveTodos } from "./hooks/useSaveTodos"
+import styles from './App.module.css'
+import { Header } from "./components/Header"
 
 function App() {
   useSaveTodos()
 
   return (
-    <h1>Hello</h1>
+    <div className={styles.container}>
+      <header>
+        <Header />
+      </header>
+    </div>
   )
 }
 
