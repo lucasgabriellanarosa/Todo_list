310bba9e6aa0affe685882fbb9772758ffd0ec62
Lucas Gabriel de Lana Rosa <lucas.gabriel.lr2112@gmail.com>
2025-04-06 22:27:12 -0300

    finishing project

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,6 +1,8 @@
 import { useContext } from 'react'
 import styles from './TodoList.module.css'
 import { TodoContext } from '../../contexts/TodoContext'
+import { TodoItem } from '../TodoItem'
+import EmptyIcon from '../../assets/clipboard.svg?react'
 
 export const TodoList = () => {
 
@@ -17,6 +19,27 @@ export const TodoList = () => {
                     </div>
                 </div>
             }
+
+            {todos.length < 1 &&
+                <div className={styles.emptyContainer}>
+                    <EmptyIcon />
+                    <p className={styles.emptyLabel}>
+                        <strong>Você ainda não tem tarefas cadastradas.</strong>
+                        <br />
+                        Crie tarefas e organize seus itens a fazer
+                    </p>
+                </div>
+            }
+
+            <div className={styles.tasksContainer}>
+                {todos.map((data, key) => (
+                    <TodoItem 
+                        key={key}
+                        data={data}
+                        dataIndex={key}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
